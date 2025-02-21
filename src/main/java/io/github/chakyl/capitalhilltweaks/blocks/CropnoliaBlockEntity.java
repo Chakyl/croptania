885f7b553c883d3d290f8cf99169bb569a8ac7a0
Chakyl <chakyl1127@gmail.com>
2025-02-20 23:39:53 -0500

    Cleanup

--- src/main/java/io/github/chakyl/capitalhilltweaks/blocks/CropnoliaBlockEntity.java
+++ src/main/java/io/github/chakyl/capitalhilltweaks/blocks/CropnoliaBlockEntity.java
@@ -2,13 +2,11 @@ package io.github.chakyl.capitalhilltweaks.blocks;
 
 import io.github.chakyl.capitalhilltweaks.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
-import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.block_entity.GeneratingFlowerBlockEntity;
 import vazkii.botania.api.block_entity.RadiusDescriptor;
 import vazkii.botania.common.handler.BotaniaSounds;
@@ -30,7 +28,7 @@ public class CropnoliaBlockEntity extends GeneratingFlowerBlockEntity {
     @Override
     public void tickFlower() {
         super.tickFlower();
-        if ( ticksExisted % 100 == 0 && !getLevel().isClientSide) {
+        if ( ticksExisted % 100 == 0 ) {
             BlockPos flowerPos = this.getBlockPos();
             boolean foundCrop = false;
             for(BlockPos blockpos : BlockPos.betweenClosed(flowerPos.offset(-RANGE, 0, -RANGE), flowerPos.offset(RANGE, 1, RANGE))) {
@@ -40,18 +38,20 @@ public class CropnoliaBlockEntity extends GeneratingFlowerBlockEntity {
                     int age = cropBlock.getMaxAge();
                     addMana(manaForAge(age));
                     level.setBlock(blockpos, cropBlock.getStateForAge(0), 2);
-                    int times = 8 * (int) Math.pow(2, age);
-                    for (int j = 0; j < times; ++j) {
-                        float f = level.random.nextFloat() * (float) Math.PI * 2.0F;
-                        float f1 = level.random.nextFloat() * 0.5F + 0.5F;
-                        float f2 = Mth.sin(f) * age * 0.5F * f1;
-                        float f3 = Mth.cos(f) * age * 0.5F * f1;
-                        float f4 = level.random.nextFloat() * age * 0.5F * f1;
-                        level.addParticle(ParticleTypes.ITEM_SLIME, blockpos.getX() + f2, 1 + f4, blockpos.getZ() + f3, 0.0D, 0.0D, 0.0D);
+                    for (int i = 0; i < 20; ++i) {
+                        Vec3 offset = crop.getOffset(level, blockpos);
+                        double x = blockpos.getX() + offset.x;
+                        double y = blockpos.getY() + offset.y;
+                        double z = blockpos.getZ() + offset.z;
+
+                        SparkleParticleData data = SparkleParticleData.sparkle(level.random.nextFloat(), 0, 170, 0, 5);
+
+                        level.addParticle(data, x + Mth.randomBetween(level.random, 0, 0.9f), y + Mth.randomBetween(level.random, 0.2f, 0.4f), z + Mth.randomBetween(level.random, 0, 0.9f), 0, 0, 0);
                     }
                 }
             }
             if (foundCrop) level.playSound(null, flowerPos, BotaniaSounds.terraBlade, SoundSource.BLOCKS, 1F, 1F);
+            sync();
         }
     }
 
